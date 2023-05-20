406ea00a56fea4b26fe653ad93713e57b9c5ffcd
saymanrifat <98202630+saymanrifat@users.noreply.github.com>
2023-05-20 23:47:18 +0600

    git v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
@@ -23,10 +25,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  void getRequest() async {
+    var uri = Uri.parse("https://reqres.in/api/users?page=2");
+    var responce = await http.get(uri);
+    print(responce.body.toString());
+  }
+
+  void postRequest() async {
+    var uri = Uri.parse('https://reqres.in/api/login');
+    var data = {"email": "eve.holt@reqres.in", "password": "cityslicka"};
+    var reseonce = await http.post(uri, body: data);
+    print(reseonce.body.toString());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    postRequest();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Center()),
+      body: SafeArea(child: Container()),
     );
   }
 }
